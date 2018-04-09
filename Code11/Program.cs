4f47a034cb053340f658311efc0a141a8870e39b
VomV <vivek.vkr.rawat@gmail.com>
2018-04-10 04:08:57 +0530

    Fifth

--- Code11/Program.cs
+++ Code11/Program.cs
@@ -9,10 +9,34 @@ namespace Code11
         static void Main(string[] args)
         {
 
-            int i = 0;
-            IsPrime(i);
+            int k = 1;
+            int i = 2, Sum = 0;
+            while(k <= 500)
+            {
+                if(IsPrime(i))
+                {
+                    Sum += i;
+                    k++;
+                    Console.WriteLine(i);
+                }
+                
+                i++;
+            }
+            Console.WriteLine(k);
+            Console.WriteLine(i);
+            Console.WriteLine("Sum of first 500 prime Nos: {0}", Sum);
+        }
 
+        public static bool IsPrime(int N)
+        {
+            for(int i=2; i<=Math.Sqrt(N); i++)
+            {
 
+                if (N % i == 0)
+                    return false;
+
+            }
+            return true;
         }
     }
 }
